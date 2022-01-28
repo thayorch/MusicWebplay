7a47d54dfd4cdeaca16796cab39e36ef8e34dc75
ZaICEaZ <86808076+ZaICEaZ@users.noreply.github.com>
2022-01-28 08:49:39 +0700

    Import

--- MusicWeb/src/js/play.js
+++ MusicWeb/src/js/play.js
@@ -3,18 +3,18 @@ var icon = document.getElementById("icon");
     
         if(!icon.onclick){
                 mySong.play();             //pause == หยุดตอนนั้นๆ
-                icon.src= "/src/img/pause.png";
+                icon.src= "src/img/pause.png";
         }
 
     icon.onclick = function(){
 
         if (mySong.paused) {            //paused == หยุดไปแล้ว
             mySong.play();
-            icon.src= "/src/img/pause.png";
+            icon.src= "src/img/pause.png";
         }
         else{
             mySong.pause();             //pause == หยุดตอนนั้นๆ
-            icon.src= "/src/img/play.png";
+            icon.src= "src/img/play.png";
         }
 
     }
